6b49b3b5142ff985fe99b7f443c3b3a299af5df5
iljung1106 <1wndrla@naver.com>
2025-04-07 22:10:44 +0900

    changed default port

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def create_app():
 # 애플리케이션 실행
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
